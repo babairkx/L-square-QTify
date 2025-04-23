804bd7a1bf0d74a577ec9abdae28617ebdcaebef
babairkx <babairkx@gmail.com>
2025-04-23 11:14:10 +0530

    Hero

--- vite-qtify-v2/src/App.jsx
+++ vite-qtify-v2/src/App.jsx
@@ -1,13 +1,16 @@
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
-import Hero from "./components/hero/Hero";
+import Desktop1 from "./pages/desktop1/Desktop1";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
+        <Routes>
+          <Route path="/" element={ <Desktop1 /> } />
+        </Routes>
       </BrowserRouter>
     </>
   );
---
+++ vite-qtify-v2/src/pages/desktop1/Desktop1.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Hero from "../../components/hero/Hero";
+
+const Desktop1 = () => {
+  return <Hero />;
+};
+export default Desktop1;
